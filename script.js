c3c5cbdbb12f8e2a2bae3d4ee9f02a80a49d2756
jrme421 <jeromepchallet@gmail.com>
2024-02-04 16:13:39 +0900

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("heladsf");
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
+}
+console.log("bills", bills);
+console.log("tips", tips);
+console.log("totals", totals);
+
+function calAverage(arr) {
+  //remember to asign 0 first
+  //remember u can use an outside var as long as its declared before the loop
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    console.log("sum: ", sum);
+  }
+  let average = sum / arr.length;
+  return average;
+}
+
+console.log(calAverage(bills));
+console.log(calAverage(tips));
+console.log(calAverage(totals));
